c9f122c0c0044f8743252539500e84af80d3d59d
fabiopulcinelli <fabiopulcinelli2k2@gmail.com>
2022-12-01 14:05:40 +0100

    primo commit

---
+++ src/conflitti/conflittiMain.java
@@ -0,0 +1,11 @@
+package conflitti;
+
+public class conflittiMain {
+	public static void main(String[] args) {
+		System.out.println("Ciao ");
+		
+		System.out.println("come ");
+		
+		System.out.println("va?");
+	}
+}
