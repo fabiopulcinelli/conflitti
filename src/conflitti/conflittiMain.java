34ae6a3f7a06acd45fa0b58722571fddc020be82
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-12-01 14:44:06 +0100

    Added Loris

--- src/conflitti/conflittiMain.java
+++ src/conflitti/conflittiMain.java
@@ -2,7 +2,7 @@ package conflitti;
 
 public class conflittiMain {
 	public static void main(String[] args) {
-		System.out.println("Ciao Ross ");
+		System.out.println("Ciao Ross Loris ");
 		
 		System.out.println("come ");
 		
